c6590618e4d36cdecf722abc49d11b2585872b4d
Marlon Mitchell <mdmitch7@gmail.com>
2021-08-12 14:11:07 -0400

    added Redux  Logform  pages, fully complete.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Login from './Components/Login'
+import Logout from './Components/Logout'
+
 import './App.css'
+import { selectUser } from './features/userSlice'
+import { useSelector } from 'react-redux'
 
 const App = () => {
+  const user = useSelector(selectUser)
   return (
-    <div>
-      <Login />
-    </div>
+    <div>{user ? <Logout />: <Login />}</div>
   )
 }
 
--- src/Components/Logout.js
+++ src/Components/Logout.js
@@ -1,11 +1,23 @@
-import "./Login.css";
+import "./Logout.css";
 import React from 'react'
+import { logout, selectUser } from "../features/userSlice";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 const Logout = () => {
+  const user = useSelector(selectUser)
+  const dispatch = useDispatch() 
+  const handleLogout = e => {
+    e.preventDefault()
+    dispatch(logout()) 
+  }
   return (
-    <div>
-      <h1>Welcome</h1><span className="user__name">User</span>
-      <button  className="logout__button">Logout</button>
+    <div className="logout">
+      <h1>
+        Welcome
+        <span className="user__name"> {user.name}</span>
+      </h1>{" "}
+      <button  className="logout__button" onClick={ e => handleLogout(e)}>Logout</button>
     </div>
   )
 }
